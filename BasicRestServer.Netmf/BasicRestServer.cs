e3f39d4b3faceccfc3a8e06ebb3d19dab170785b
Marcin Badurowicz <ktos@ktos.info>
2014-06-04 13:50:15 +0200

    bugfix - Uri.AbsoluteUri throws InvalidOperationException on NETMF

--- BasicRestServer.Netmf/BasicRestServer.cs
+++ BasicRestServer.Netmf/BasicRestServer.cs
@@ -1,11 +1,37 @@
+#region License
+/*
+ * BasicRestServer
+ *
+ * Copyright (C) Marcin Badurowicz 2014
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ */
+#endregion
+
 using System;
-using Microsoft.SPOT;
 using System.Net;
 using System.Net.Sockets;
 using System.Collections;
 using System.Threading;
-using System.IO;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Ktos.RestServer
@@ -65,7 +91,7 @@ namespace Ktos.RestServer
                 if (context.Request.HttpMethod.ToUpper() == x.Method.ToUpper())
                 {
                     Regex r = new Regex(x.Uri);
-                    if (r.IsMatch(context.Request.Url.AbsoluteUri))
+                    if (r.IsMatch(context.Request.RawUrl))
                     {
                         x.Handler(context.Request, context.Response);
                         return;
